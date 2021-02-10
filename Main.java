711f6b57baa3eecb346cca55d547792bf6dd12b1
diegotco <diegotco@yahoo.com>
2021-02-10 12:41:02 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Main {
+    public static void main(String[] args) throws Exception {
+        System.out.println(suma(4,5));
+    }
+    
+    public static int suma(int valor1, int valor2){
+        
+       return valor1 + valor2;
+       
+    }
+}
